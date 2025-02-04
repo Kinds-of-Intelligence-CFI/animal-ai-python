6bc7e97220cc35bbd6ee3d1314bc01c105946956
ProFrenchToast <66527432+ProFrenchToast@users.noreply.github.com>
2025-02-04 14:43:15 +0000

    Made Play.py directly runnable
    Added the option to call play.py using command line arguments. this should allow new users to load and test configs with less overhead than trying to run a jupyter notebook.

--- animalai/play.py
+++ animalai/play.py
@@ -1,3 +1,4 @@
+import argparse
 import random
 
 from pathlib import Path
@@ -5,7 +6,7 @@ from pathlib import Path
 from animalai import AnimalAIEnvironment, arenas
 from animalai.executable import find_executable
 
-def play(configuration_file: str = None, env_path: str = None) -> None:
+def play(configuration_file: str = None, env_path: str = None, log_path: str = None) -> None:
     """
     Load a config file and play
     :param configuration_file: str path to a yaml configuration. Plays animalai.arenas.GoodGoal_Random.yml by default
@@ -19,6 +20,7 @@ def play(configuration_file: str = None, env_path: str = None) -> None:
         base_port=5005 + random.randint(0, 1000),
         arenas_configurations=configuration_file if configuration_file is not None else arenas.GOOD_GOAL_RANDOM_POS,
         play=True,
+        log_folder=log_path,
     )
 
     print("Press Q in the Unity window then Ctrl+C in the command line to close the environment effectively.")
@@ -31,3 +33,16 @@ def play(configuration_file: str = None, env_path: str = None) -> None:
         pass
     finally:
         environment.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='This file allows the user to open and play an AnimalAI environment.')
+    parser.add_argument("--AAI_path", default=None, type=str, help="The path to the AnimalAI binary to use.")
+    parser.add_argument("--config_path", default=None, type=str, help="The path to the config file of the environment you want ot use.")
+    parser.add_argument("--log_path", default="./logs", type=str, help="The folder AnimalAI should output its logs to.")
+
+    # parse the args
+    args = parser.parse_args()
+
+    # play in the ennvironment
+    play(configuration_file=args.config_path, env_path=args.AAI_path, log_path=args.log_path)
